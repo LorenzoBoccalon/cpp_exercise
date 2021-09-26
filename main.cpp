87b2f935bc51120cf2d97e8b3252c9b94012af88
LorenzoBoccalon <47595259+LorenzoBoccalon@users.noreply.github.com>
2021-09-26 13:27:25 +0200

    Abbandoned

--- main.cpp
+++ main.cpp
@@ -21,11 +21,11 @@ digit_set possible_digits(ulong number);
 
 ulong first_digit(const std::string &number_str);
 
-std::string number_to_str(ulong number);
+std::string itos(ulong number);
 
 int main() {
     auto in = parse_input();
-    auto ins = number_to_str(in);
+    auto ins = itos(in);
     auto f = first_digit(ins);
     auto l = n_digits(ins);
     std::cout << "number: " << in << std::endl;
@@ -35,6 +35,8 @@ int main() {
     for (auto d: ds) {
         std::cout << d << " ";
     }
+    std::cout << std::endl;
+
     // test 1
     if (find_next_smallest(2) == 3) {
         std::cout << "Test 1: passed" << std::endl;
@@ -53,16 +55,32 @@ int main() {
     } else {
         std::cout << "Test 3: failed" << std::endl;
     }
+    // test 4
+    if (find_next_smallest(126456) == 300000) {
+        std::cout << "Test 4: passed" << std::endl;
+    } else {
+        std::cout << "Test 4: failed" << std::endl;
+    }
 }
 
-ulong find_next_smallest(ulong number) {
+ulong find_next_smallest(ulong number) { // 1234
     ulong next = 0;
-
+//    std::cout << "number: " << number << std::endl;
+    // first digits = min possible digits diff from 0
+    auto ds = possible_digits(number); // 0 5 6 7 8 9
+    auto it = ds.begin();
+    auto fd = first_digit(itos(number)); // 1
+//    std::cout << "fd: " << fd << std::endl;
+    auto new_fd = *it; // 0
+    while (it != ds.end() && new_fd < fd) {
+        new_fd = *++it;
+    }
+//    std::cout << "new_fd: " << new_fd << std::endl;
 
     return next;
 }
 
-std::string number_to_str(ulong number) {
+std::string itos(ulong number) {
     return std::to_string(number);
 }
 
